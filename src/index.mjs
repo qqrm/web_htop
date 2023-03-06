cd81de45bbe7c2eaf863b2c6c45ede1d6c4f3f14
qqrm <qqrm@mail.ru>
2023-03-07 02:03:52 +0300

    update cpu usag in background, prettyfing front

--- src/index.mjs
+++ src/index.mjs
@@ -6,16 +6,20 @@ const html = htm.bind(h);
 
 function App(props) {
     return html`
+        <h1>Cpu load</h1>
         <div>
         ${props.cpus.map((cpu) => {
-        return html` <div>${cpu.toFixed(2)}% usage</div>`;
+        return html`<div class="bar">
+            <div class="bar-inner" style="width: ${cpu}%"></div>
+            <label>${cpu.toFixed(2)}% usage</label>
+        </div>`;
     })}
     </div>`;
 }
 
 let i = 0;
 
-setInterval(async () => {
+let update = async () => {
     let response = await fetch("/api/cpus");
 
     if (response.status !== 200) {
@@ -26,4 +30,5 @@ setInterval(async () => {
 
     render(html`<${App} cpus=${json}  </${App}>`, document.body);
 
-}, 1000)
+}
+setInterval(update, 200)
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,39 @@ use sysinfo::{CpuExt, System, SystemExt};
 
 #[tokio::main]
 async fn main() {
+    let app_state = AppState::default();
+
     let router = Router::new()
         .route("/", get(root))
         .route("/index.mjs", get(index_mjs))
+        .route("/index.css", get(index_css))
         .route("/api/cpus", get(get_cpu_load))
-        .with_state(AppState {
-            sys: Arc::new(Mutex::new(System::new())),
-        });
+        .with_state(app_state.clone());
+
+    tokio::task::spawn_blocking(move || {
+        let mut sys = System::new();
+        loop {
+            sys.refresh_cpu();
+            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+
+            {
+                let mut cpus = app_state.cpus.lock().unwrap();
+                *cpus = v;
+            }
+
+            std::thread::sleep(System::MINIMUM_CPU_UPDATE_INTERVAL);
+        }
+    });
+
     let server = Server::bind(&"0.0.0.0:8081".parse().unwrap()).serve(router.into_make_service());
     let addr = server.local_addr();
-    println!("Listening on {addr}");
+    println!("Listening on http://{addr}");
     server.await.unwrap();
 }
 
-#[derive(Clone)]
+#[derive(Default, Clone)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    cpus: Arc<Mutex<Vec<f32>>>,
 }
 
 #[axum::debug_handler]
@@ -43,11 +60,18 @@ async fn index_mjs() -> impl IntoResponse {
 }
 
 #[axum::debug_handler]
+async fn index_css() -> impl IntoResponse {
+    // Development solution, not for production
+    let css = tokio::fs::read_to_string("src/index.css").await.unwrap();
+
+    Response::builder()
+        .header("content-type", "text/css")
+        .body(css)
+        .unwrap()
+}
+
+#[axum::debug_handler]
 async fn get_cpu_load(State(state): State<AppState>) -> impl IntoResponse {
-    let mut sys = state.sys.lock().unwrap();
-    sys.refresh_cpu();
-
-    let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
-
+    let v = state.cpus.lock().unwrap().clone();
     Json(v)
 }
